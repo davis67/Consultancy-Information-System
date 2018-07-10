b1573e6bdd7b3de3e3e28e8ee5b88faa8f93bb71
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2018-07-10 12:30:15 +0300

    refactor: Use bulk inserts for multiple entries

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,42 +10,45 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $teams = [
+            [
+                'team' => 'TCS',
+                'team_leader' => 'Mugisha Vivacious',
+                'team_description' => 'Teschnical Support Services',
+            ],
+            [
+                'team' => 'MCS',
+                'team_leader' => 'Rebecca Namayanja',
+                'team_description' => 'Management Consulting Services',
+            ],
+            [
+                'team' => 'BDS',
+                'team_leader' => 'William Kilama',
+                'team_description' => 'Business Management Services',
+            ],
+            [
+                'team' => 'HTA',
+                'team_leader' => 'Connie Katusiime',
+                'team_description' => 'Hanighton Trainning Academy',
+            ],
+            [
+                'team' => 'HCM',
+                'team_leader' => 'Betty Namutebbi',
+                'team_description' => 'Human Commission Management',
+            ],
+            [
+                'team' => 'CSS',
+                'team_leader' => 'Leila Busingye',
+                'team_description' => 'Co-operate Support Services',
+            ],
+            [
+                'team' => 'DCS',
+                'team_leader' => 'Agaba Davis',
+                'team_description' => 'Development Consulting Services',
+            ],
+        ];
 
-        Team::create([
-            'team' => 'TCS',
-            'team_leader' => 'Mugisha Vivacious',
-            'team_description' => 'Teschnical Support Services',
-        ]);
-        Team::create([
-            'team' => 'MCS',
-            'team_leader' => 'Rebecca Namayanja',
-            'team_description' => 'Management Consulting Services',
-        ]);
-        App\Team::create([
-            'team' => 'BDS',
-            'team_leader' => 'William Kilama',
-            'team_description' => 'Business Management Services',
-        ]);
-        App\Team::create([
-            'team' => 'HTA',
-            'team_leader' => 'Connie Katusiime',
-            'team_description' => 'Hanighton Trainning Academy',
-        ]);
-        App\Team::create([
-            'team' => 'HCM',
-            'team_leader' => 'Betty Namutebbi',
-            'team_description' => 'Human Commission Management',
-        ]);
-        App\Team::create([
-            'team' => 'CSS',
-            'team_leader' => 'Leila Busingye',
-            'team_description' => 'Co-operate Support Services',
-        ]);
-        App\Team::create([
-            'team' => 'DCS',
-            'team_leader' => 'Agaba Davis',
-            'team_description' => 'Development Consulting Services',
-        ]);
+        App\Team::Insert(static::addTimeStamps($teams));
 
         $user = App\User::create([
             'name' => 'Davis Agaba',
@@ -59,6 +62,7 @@ class DatabaseSeeder extends Seeder
             'is_permitted' => 5,
             'employeeNo' => 192,
         ]);
+
         App\Profile::create([
             'user_id' => $user->id,
             'avatar' => 'uploads/1.jpeg',
@@ -66,17 +70,32 @@ class DatabaseSeeder extends Seeder
             'primary_address' => 'Kampala',
         ]);
 
-        App\Usergroup::create([
-            'name' => 'intern',
-            'description' => 'internship level',
-        ]);
-        App\Usergroup::create([
-            'name' => 'consultant',
-            'description' => 'fully employee',
-        ]);
-        App\Usergroup::create([
-            'name' => 'Assistant Manager',
-            'description' => 'junior manager',
-        ]);
+        $groups = [
+            [
+                'name' => 'intern',
+                'description' => 'internship level',
+            ],
+            [
+                'name' => 'consultant',
+                'description' => 'fully employee',
+            ],
+
+            [
+                'name' => 'Assistant Manager',
+                'description' => 'junior manager',
+            ],
+        ];
+
+        App\Usergroup::insert(static::addTimeStamps($groups));
+    }
+
+    public static function addTimeStamps($data)
+    {
+        return collect($data)->map(function ($datum) {
+            $datum['created_at'] = now();
+            $datum['updated_at'] = now();
+
+            return $datum;
+        })->toArray();
     }
 }
